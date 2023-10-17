c9def04bd9c212f0766e91683ddcaf8897871d0d
rados4y <rados4y@o2.pl>
2023-10-17 23:04:13 +0200

    fix in script

--- ci/install_go.sh
+++ ci/install_go.sh
@@ -4,10 +4,14 @@ if command -v yum &> /dev/null
 then
     yum -y update
     yum -y install wget
-    wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
+    wget -q https://golang.org/dl/go1.18.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
     echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
     source $HOME/.bash_profile
+    export PATH=/usr/local/go/bin:$PATH
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/go
+    go version
 else
     echo "Unsupported OS - yum not available"
     exit 1
