3db876313c69035b8c1d8160d8522aad25566b30
rados4y <49685748+rados4y@users.noreply.github.com>
2023-10-18 10:01:32 +0200

    install 32bit of go

--- ci/install_go.sh
+++ ci/install_go.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
+architecture=$(uname -m)
+
+case $architecture in
+    x86_64)
+        echo "64-bit system detected"
+        ;;
+    i386|i486|i586|i686)
+        echo "32-bit system detected"
+        ;;
+    *)
+        echo "Unknown architecture detected: $architecture"
+        ;;
+esac
+
 if command -v yum &> /dev/null
 then
+    architecture=$(uname -m)
+    url=""
+    case $architecture in
+        x86_64)
+            url="https://golang.org/dl/go1.18.linux-amd64.tar.gz"
+            ;;
+        i386|i486|i586|i686)
+            url="https://go.dev/dl/go1.21.3.linux-386.tar.gz"
+            ;;
+        *)
+            echo "Unknown architecture: $architecture"
+            exit 1
+            ;;
+    esac
     yum -y update
     yum -y install wget
-    wget -q https://golang.org/dl/go1.18.linux-amd64.tar.gz
+    wget -q "$url"
     tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
     echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
     source $HOME/.bash_profile
