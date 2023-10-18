b358c2e5951d6dbe1f9cecbd1a80bfa647b4bce0
rados4y <49685748+rados4y@users.noreply.github.com>
2023-10-18 10:51:57 +0200

    uname -a in go install

--- ci/install_go.sh
+++ ci/install_go.sh
@@ -14,35 +14,27 @@ case $architecture in
         ;;
 esac
 
-if command -v yum &> /dev/null
-then
-    architecture=$(uname -m)
-    url=""
-    case $architecture in
-        x86_64)
-            url="https://golang.org/dl/go1.18.linux-amd64.tar.gz"
-            ;;
-        i386|i486|i586|i686)
-            url="https://go.dev/dl/go1.21.3.linux-386.tar.gz"
-            ;;
-        *)
-            echo "Unknown architecture: $architecture"
-            exit 1
-            ;;
-    esac
-    yum -y update
-    yum -y install wget
-    wget -q "$url" -O go.tar.gz
-    tar -C /usr/local -xzf go.tar.gz
-    echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
-    source $HOME/.bash_profile
-    export PATH=/usr/local/go/bin:$PATH
-    export GOROOT=/usr/local/go
-    export GOPATH=$HOME/go
-    go version
-    which go
-    ls /usr/local/go/bin/
-else
-    echo "Unsupported OS - yum not available"
-    exit 1
-fi
+architecture=$(uname -m)
+url=""
+case $architecture in
+    x86_64)
+        url="https://golang.org/dl/go1.18.linux-amd64.tar.gz"
+        ;;
+    i386|i486|i586|i686)
+        url="https://go.dev/dl/go1.21.3.linux-386.tar.gz"
+        ;;
+    *)
+        echo "Unknown architecture: $architecture"
+        exit 1
+        ;;
+esac
+uname -a
+wget -q "$url" -O go.tar.gz
+tar -C /usr/local -xzf go.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+export PATH=/usr/local/go/bin:$PATH
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+go version
+which go
